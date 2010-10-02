d03707560ebafd7c4a6c77a4c9f214836a318109
surftheair <surftheair@bfc67b58-a102-776f-432c-be493ff401cd>
2010-10-02 08:29:57 +0000

    Lyrics of the current song added. Cache variables to local file, so no need to fetch API every time the client send a request

--- icecast.php
+++ icecast.php
@@ -1,20 +1,10 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head profile="http://gmpg.org/xfn/11">
-<title>Icecast Now Playing Script</title>
-</head>
-<body>
-<p>
-Raw PHP code from phil@simplegaming.net, modified by Jude (<a href="mailto:surftheair@gmail.com">surftheair@gmail.com</a>)
-</p>
 <?php 
-/* Raw PHP code from phil@simplegaming.net, modified by Jude (surftheair@gmail.com)
-/* 
- * SCRIPT CONFIGURATIONS 
-*/ 
-$SERVER = 'http://example.com:8000'; //URL TO YOUR ICECAST SERVER 
-$STATS_FILE = '/status.xsl?mount=your_mount_point'; //PATH TO STATUS.XSL PAGE OF YOUR MOUNT POINT
-$LASTFM_API= 'your_lastfm_api_key'; //YOUR API KEY FROM LAST.FM, GET AT http://www.last.fm/api/account
+// By Jude (surftheair@gmail.com), some code from phil@simplegaming.net
+
+// configurations
+$SERVER = 'http://zlz-stream10.streaming.init7.net:80'; //URL TO YOUR ICECAST SERVER 
+$STATS_FILE = '/status.xsl?mount=/1/rsp/mp3_128'; //PATH TO STATUS.XSL PAGE OF YOUR MOUNT POINT
+$LASTFM_API= 'dd40af1da9977c679287dd2d78f017b4'; //YOUR API KEY FROM LAST.FM, GET AT http://www.last.fm/api/account
 
 ///////////////////// END OF CONFIGURATION --- DO NOT EDIT BELOW THIS LINE \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ 
 
@@ -77,53 +67,68 @@ $radio_info['current_song'] = $temp_array[9];
 
 $x = explode(" - ",$temp_array[9]); 
 $radio_info['now_playing']['artist'] = $x[0]; 
-$radio_info['now_playing']['track'] = $x[1]; 
+$radio_info['now_playing']['track'] = $x[1];
+
+//check if the current song has changed
+$last_song = file_get_contents("cache/history.txt");
+
+if($last_song !== $radio_info['current_song']){
+
+//if the current song has changed, refetch information of the new track from remote APIs 
+$fp=fopen("cache/history.txt","w");
+fwrite($fp,$radio_info['current_song']);
+fclose($fp);
 
 //get information of the current song use last.fm's API, by Jude
 $xml_request_url = 'http://ws.audioscrobbler.com/2.0/?method=track.getinfo&artist='.$radio_info['now_playing']['artist'].'&track='.$radio_info['now_playing']['track'].'&api_key='.$LASTFM_API;
 $xml = new SimpleXMLElement($xml_request_url, null, true);
 	if($xml->track->album->image){
-		$album_art = $xml->track->album->image[2];}
-	else{$album_art = '/static/default.jpg';}
+		$album_art = $xml->track->album->image[2];
+		}
+	else{$album_art = '/static/default.jpg';
+	}
 	if ($xml->track->wiki->summary){
-		$track_info = $xml->track->wiki->summary;}
+		$track_info = $xml->track->wiki->summary;
+		}
 	else{$track_info = "No information found for this track, try searching for <a href='http://www.google.com/search?q=".$radio_info['current_song']."'>".$radio_info['current_song']."</a> on Google";}
 $track_lastfm_url = $xml->track->url;
 $artist_lastfm_url = $xml->track->artist->url;
 	if($xml->track->album->title){
 		$album_title = $xml->track->album->title;
-		$album_lastfm_url = $xml->track->album->url;}
+		$album_lastfm_url = $xml->track->album->url;
+		}
 	else{
 		$album_title = 'Not found';
-		$album_lastfm_url = 'http://www.google.com/search?q='.$radio_info['current_song']	;}
+		$album_lastfm_url = 'http://www.google.com/search?q='.$radio_info['current_song'];
+		}
 $track_download = 'http://www.google.cn/music/search?q='.$radio_info['current_song'];
 
-//cache album art images
+//cache album art images to local server
 $filename = end(explode('/',$album_art));
 $local_album_art_uri = '/cache/'.$filename;
-$local_album_art = '/home/wwwroot/icecast/cache/'.$filename;
+$local_album_art = 'cache/'.$filename;
 if (!is_file($local_album_art)){
 copy($album_art,$local_album_art );}
-?>
 
-<!--output, edit the followings as you like, this is just an example -->
-Station title: <?php echo ($radio_info['title']);?><br />
-Description: <?php echo ($radio_info['description']);?><br />
-Conten type: <?php echo ($radio_info['content_type']);?><br />
-Mount start: <?php echo ($radio_info['mount_start']);?><br />
-Bitrate: <?php echo ($radio_info['bit_rate']);?><br />
-Current listners: <?php echo ($radio_info['listeners']);?><br />
-Peak listeners: <?php echo ($radio_info['most_listeners']);?><br />
-Genre: <?php echo ($radio_info['genre']);?><br />
-Station URL: <?php echo ($radio_info['url']);?><br />
-Now palying: <?php echo ($radio_info['current_song']);?><br />
-Artist: <?php echo($radio_info['now_playing']['artist']);?><br />
-Track: <?php echo($radio_info['now_playing']['track']);?><br />
-Album Art: <img src="<?php echo($album_art);?>"/><br />
-Track introduction: <?php echo($track_info);?><br />
-Track URL on last.fm: <?php echo($track_lastfm_url);?><br />
-Artist URL on last.fm: <?php echo($artist_lastfm_url);?><br />
-Album title: <?php echo($album_title);?><br />
-Album URL on last.fm: <?php echo($album_lastfm_url);?><br />
-Download link on Google Music(China): <?php echo($track_download);?><br />
-Local cache of album art: <?php echo($local_album_art_uri);?></br>
+//get lyrics from chartlyrics.com's API
+$xml_request_url = 'http://api.chartlyrics.com/apiv1.asmx/SearchLyricDirect?artist='.$radio_info['now_playing']['artist'].'&song='.$radio_info['now_playing']['track'];
+$xml = new SimpleXMLElement($xml_request_url, null, true);
+if($xml->LyricId == '0'){
+$track_lyric = 'Lyrics not found for this track';}
+else{
+$track_lyric = $xml->Lyric;}
+
+//write variables to file
+$variables = "<?php \$local_album_art_uri = \"$local_album_art_uri\";\$track_info=\"$track_info\" ;\$album_title= \"$album_title\";\$album_lastfm_url = \"$album_lastfm_url\";\$track_lastfm_url=\"$track_lastfm_url\";\$artist_lastfm_url=\"$artist_lastfm_url\";\$track_lyric=\"$track_lyric\";\$album_art=\"$album_art\";\$track_download=\"$track_download\";?>";
+$fp=fopen("cache/variales.php","w");
+fwrite($fp,$variables);
+fclose($fp);
+$source = 'remote API';
+}
+
+//if the current song is not changed, get the information from local file, so no need to frequently fetch form API again, which may result in baning
+else{
+include("cache/variales.php");
+$source = 'cached file';
+}
+?>
