349bb172d33bd33148a8f93ad56a81a4d412603c
surftheair <surftheair@bfc67b58-a102-776f-432c-be493ff401cd>
2011-09-28 04:59:58 +0000

    rewrote most code new features added:   1. get buy links on Amazon, iTunes and 7digital   2. store radio's play history on file

---
+++ config.php
@@ -0,0 +1,14 @@
+﻿<?php
+
+//error_reporting(0);
+define(SERVER, 'http://127.0.0.1:8000');//your icecast server address, without the ending "/"
+define(MOUNT, '/stream'); //your radio's mount point, with the leading "/"
+define(LAST_FM_API, ''); //your last.fm API key, get from http://www.last.fm/api/account
+define(DEFAULT_ALBUM_ART, 'cache/default.jpg');//the default album art image, will be used if failed to get from last.fm's API
+define(GET_TRACK_INFO, true); //get information of the current song from last.fm
+define(GET_ALBUM_INFO, true); //get extra information of the album from last.fm, if enabled, may increase script execute time
+define(GET_ARTIST_INFO, true); //get extra information of the artist from last.fm, if enabled, may increase script execute time
+define(GET_TRACK_BUY_LINK, true); //get buy links on Amazon, iTune and 7digital
+define(GET_LYRICS, true); //get lyrics of the current song using chartlyrics.com's API
+define(CACHE_ALBUM_ART, true);//cache album art images to local server
+define(RECORD_HISTORY, true);//record play history of your radio
--- example.php
+++ example.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head profile="http://gmpg.org/xfn/11">
 <title>Icecast Now Playing Script</title>
@@ -7,61 +7,12 @@
 <p>
 Code by Jude (<a href="mailto:surftheair@gmail.com">surftheair@gmail.com</a>)
 </p>
-<?php include("icecast.php");?>
-<table border="1">
-<tr><td>$status</td><td><?php echo ($status);?></td></tr>
+<pre>
 <?php
-foreach($stream_info as $key => $value){
-	echo '<tr><td>$stream_info[\''.$key.'\']</td><td>';
-	if(is_array($value)){
-		foreach($value as $key_ => $value_){
-			echo $value_.'<br />';
-		}
-	}
-	else{
-		echo $value;
-	}
-	echo '</td></tr>';
-}
-foreach($track_info as $key => $value){
-	echo '<tr><td>$track_info[\''.$key.'\']</td><td>';
-	if(is_array($value)){
-		foreach($value as $key_ => $value_){
-			echo $value_.'<br />';
-		}
-	}
-	else{
-		echo $value;
-	}
-	echo '</td></tr>';
-}
-foreach($album_info as $key => $value){
-	echo '<tr><td>$album_info[\''.$key.'\']</td><td>';
-	if(is_array($value)){
-		foreach($value as $key_ => $value_){
-			echo $value_.'<br />';
-		}
-	}
-	else{
-		echo $value;
-	}
-	echo '</td></tr>';
-}
-foreach($artist_info as $key => $value){
-	echo '<tr><td>$artist_info[\''.$key.'\']</td><td>';
-	if(is_array($value)){
-		foreach($value as $key_ => $value_){
-			echo $value_.'<br />';
-		}
-	}
-	else{
-		echo $value;
-	}
-	echo '</td></tr>';
-}
+include('icecast.php');
+print_r($stream);
 ?>
-<tr><td>$source</td><td><?php echo $source;?></td></tr>
-</table>
+</pre>
 </body>
 </html>
 
